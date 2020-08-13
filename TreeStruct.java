c61c1a9caa1e56e00870add44c511fa1ded212fb
Prathamesh <prathamesh@sayalis-MacBook-Air.local>
2020-08-13 23:26:40 +0530

    First Commit

---
+++ TreeStruct.java
@@ -0,0 +1,106 @@
+class TreeNode<T extends Comparable<T>>{
+
+     T data;
+     TreeNode leftNode;
+     TreeNode rightNode;
+
+
+    public void insert(T nodeData){
+
+        if(nodeData.compareTo(data)> 0){
+            if(this.rightNode == null){
+
+                this.rightNode = new TreeNode(nodeData);
+            }
+            else{
+
+                this.rightNode.insert(nodeData);
+            }
+        }else if(nodeData.compareTo(data)<0){
+
+            if(this.leftNode == null){
+
+                this.leftNode = new TreeNode(nodeData);
+            }
+            else{
+
+                this.leftNode.insert(nodeData);
+            }
+        }
+    }
+
+    public TreeNode(T data){
+
+        this.data = data;
+        this.leftNode = this.rightNode = null;
+    }
+
+
+     public T getNode(TreeNode<T> node){
+
+        
+            return node.data;
+        
+    }
+
+    public TreeNode getLeftNode(TreeNode node){
+
+        if(!node.leftNode.equals(null)){
+
+            return node.leftNode;
+        }else{
+            return null;
+        }
+    }
+
+    public TreeNode getRightNode(TreeNode node){
+
+        if(!node.rightNode.equals(null)){
+
+            return node.rightNode;
+        }else{
+            return null;
+        }
+    }
+
+}
+
+
+class TreeStruct<T extends Comparable<T>>{
+
+    private TreeNode<T> root;
+
+    public TreeStruct(){
+        root = null;
+    }
+
+
+
+    public void insertNode(T node){
+        if(this.root == null){
+            this.root = new TreeNode(node);
+        }
+        else{
+            root.insert(node);
+        }
+    }
+
+    public void inOrderTraversal(){
+
+        inOrderTraversalHelper(root);
+
+    }
+
+    private void inOrderTraversalHelper(TreeNode<T> helper){
+
+        if(helper == null){
+
+            return;
+        }
+
+        inOrderTraversalHelper(helper.leftNode);
+        System.out.println(helper.data);
+        inOrderTraversalHelper(helper.rightNode);
+    }
+
+}
---
+++ TreeTrav.java
@@ -0,0 +1,31 @@
+import java.security.SecureRandom;
+
+class TreeTrav{
+
+    public static void main(String args[]){
+
+
+
+        TreeStruct<Integer> tree1 = new TreeStruct<Integer>();
+
+        SecureRandom random = new SecureRandom();
+
+        System.out.printf("Inserting the following values %n");
+
+        for(int i = 0; i < 10; i++){
+
+            int value = random.nextInt(100);
+
+            tree1.insertNode(value);
+
+            System.out.printf("Inserting %d %n", value);
+        }
+
+
+        System.out.println("The inorder traversal of this tree is ");
+        tree1.inOrderTraversal();
+
+    }
+
+
+}
