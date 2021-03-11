91504bf59b3edacac4d1b41578fcae852760c72a
unknown <prathuberi@gmail.com>
2021-03-11 16:58:10 +0530

    DFS

---
+++ Graphs/ADTs/DepthFirstSearch.java
@@ -0,0 +1,34 @@
+package ADTs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class DepthFirstSearch {
+
+    public static void main(String args[]) throws IOException {
+
+
+        GraphStructure graph;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the number of vertices");
+        int vertices = Integer.parseInt(br.readLine());
+        System.out.println("Enter the number of edges");
+        int edges = Integer.parseInt(br.readLine());
+        graph = new GraphStructure(vertices);
+        System.out.println("Enter the vertices");
+        for (int i = 0; i < edges; i++) {
+            Integer[] edge = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.parseInt(x)).boxed().toArray(Integer[]::new);
+            graph.addEdge(edge[0], edge[1]);
+
+        }
+
+        //   graph.printGraphStructure();
+
+        DepthFirstSearchHelper dfs = new DepthFirstSearchHelper(graph);
+        dfs.depthFirstSearch(0);
+        Arrays.stream(dfs.edgeTo).forEach(System.out::println);
+
+    }
+}
---
+++ Graphs/ADTs/DepthFirstSearchHelper.java
@@ -0,0 +1,30 @@
+package ADTs;
+
+import java.util.Iterator;
+
+public class DepthFirstSearchHelper {
+
+    boolean[] visited;
+    int[] edgeTo;
+    GraphStructure graph;
+
+    DepthFirstSearchHelper(GraphStructure graph) {
+        this.graph = graph;
+        int vertices = graph.vertices;
+        visited = new boolean[vertices];
+        edgeTo = new int[vertices];
+    }
+
+    void depthFirstSearch(int e) {
+
+        visited[e] = true;
+        for (Iterator<Integer> it = this.graph.getIteratorForVertex(e); it.hasNext(); ) {
+            int ee = it.next();
+            if (!visited[ee]) {
+                depthFirstSearch(ee);
+                edgeTo[ee] = e;
+            }
+        }
+
+    }
+}
