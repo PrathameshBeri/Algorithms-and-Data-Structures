ee11121abe0905aaf5221c45dd97b0267a2dd814
unknown <prathuberi@gmail.com>
2021-03-12 00:08:24 +0530

    ConnectedComponents

--- Graphs/ADTs/BreadthFirstSearchHelper.java
+++ Graphs/ADTs/BreadthFirstSearchHelper.java
@@ -7,25 +7,26 @@ public class BreadthFirstSearchHelper {
     boolean[] visited;
     int[] edgeTo;
     GraphStructure graph;
+    int vertices;
 
     BreadthFirstSearchHelper(GraphStructure graph) {
         this.graph = graph;
-        int vertices = graph.vertices;
+        vertices = graph.vertices;
         visited = new boolean[vertices];
         edgeTo = new int[vertices];
     }
 
 
-    void breadthFirstSearch(int node){
+    void breadthFirstSearch(int node) {
         Queue<Integer> queue = new Queue<>();
         queue.enqueue(node);
         visited[node] = true;
         edgeTo[node] = node;
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int n = queue.dequeue();
-            for(Iterator<Integer> it = graph.getIteratorForVertex(n); it.hasNext();){
+            for (Iterator<Integer> it = graph.getIteratorForVertex(n); it.hasNext(); ) {
                 int ver = it.next();
-                if(!visited[ver]) {
+                if (!visited[ver]) {
                     queue.enqueue(ver);
                     visited[ver] = true;
                     edgeTo[ver] = n;
@@ -36,5 +37,9 @@ public class BreadthFirstSearchHelper {
 
     }
 
+    void clearBFSResults() {
+        visited = new boolean[vertices];
+        edgeTo = new int[vertices];
+    }
 
 }
---
+++ Graphs/ADTs/ConnectedComponents.java
@@ -0,0 +1,40 @@
+package ADTs;
+
+public class ConnectedComponents {
+
+    boolean[] marked;
+    int[] connectedGroup;
+    GraphStructure graph;
+    BreadthFirstSearchHelper bfsHelper;
+
+    ConnectedComponents(GraphStructure graph) {
+        this.graph = graph;
+        int vertices = graph.vertices;
+        marked = new boolean[vertices];
+        connectedGroup = new int[vertices];
+        bfsHelper = new BreadthFirstSearchHelper(graph);
+    }
+
+    public void computeCC(){
+        int count = 0;
+        for(int i = 0; i < graph.vertices; i++){
+
+            if(!marked[i]){
+
+                bfsHelper.breadthFirstSearch(i);
+                for(int j = 0 ; j < bfsHelper.vertices; j++ ){
+                    if(bfsHelper.visited[j]){
+                        marked[j] = true;
+                        connectedGroup[j] = count;
+                    }
+                }
+                bfsHelper.clearBFSResults();
+                count++;
+            }
+
+        }
+
+
+    }
+
+}
