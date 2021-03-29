62af6cec2be0e78613233f4caaaac47623b0b1d1
unknown <prathuberi@gmail.com>
2021-03-29 22:26:42 +0530

    Digraph structure

---
+++ Graphs/ADTs/DiGraph.java
@@ -0,0 +1,39 @@
+package ADTs;
+
+import java.util.Iterator;
+
+public class DiGraph {
+
+    int vertices;
+    Bag<Integer>[] adjacencyMatrix;
+
+    DiGraph(int vertices) {
+        this.vertices = vertices;
+        this.adjacencyMatrix = new Bag[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjacencyMatrix[i] = new Bag<>();
+        }
+    }
+
+    void addEdge(int fromEdge, int toEdge) {
+        adjacencyMatrix[fromEdge].addItem(toEdge);
+        adjacencyMatrix[toEdge].addItem(fromEdge);
+    }
+
+    Iterator getIteratorForVertex(int vertex) {
+        return adjacencyMatrix[vertex].getAll();
+    }
+
+    void printGraphStructure() {
+        int count = 0;
+        for (Bag b : adjacencyMatrix) {
+            System.out.print(count + " -> ");
+            Iterator f = b.getAll();
+            while (f.hasNext()) {
+                System.out.print(" " + f.next());
+            }
+            count++;
+            System.out.println();
+        }
+    }
+}
