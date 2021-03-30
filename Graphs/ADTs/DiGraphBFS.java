c992bfe6590b9208e905813eceb71096748f7f4a
unknown <prathuberi@gmail.com>
2021-03-30 09:06:24 +0530

    Digraph DFS and BFS

--- Graphs/ADTs/DiGraphBFS.java
+++ Graphs/ADTs/DiGraphBFS.java
@@ -3,28 +3,42 @@ package ADTs;
 import java.util.Iterator;
 
 public class DiGraphBFS {
-
     boolean[] visited;
     int[] edgeTo;
     DiGraph graph;
+    int vertices;
 
     DiGraphBFS(DiGraph graph) {
         this.graph = graph;
-        int vertices = graph.vertices;
+        vertices = graph.vertices;
         visited = new boolean[vertices];
         edgeTo = new int[vertices];
     }
 
-    void depthFirstSearch(int e) {
 
-        visited[e] = true;
-        for (Iterator<Integer> it = this.graph.getIteratorForVertex(e); it.hasNext(); ) {
-            int ee = it.next();
-            if (!visited[ee]) {
-                depthFirstSearch(ee);
-                edgeTo[ee] = e;
+    void breadthFirstSearch(int node) {
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueue(node);
+        visited[node] = true;
+        edgeTo[node] = node;
+        while (!queue.isEmpty()) {
+            int n = queue.dequeue();
+            for (Iterator<Integer> it = graph.getIteratorForVertex(n); it.hasNext(); ) {
+                int ver = it.next();
+                if (!visited[ver]) {
+                    queue.enqueue(ver);
+                    visited[ver] = true;
+                    edgeTo[ver] = n;
+                }
             }
+
         }
 
     }
+
+    void clearBFSResults() {
+        visited = new boolean[vertices];
+        edgeTo = new int[vertices];
+    }
+
 }
